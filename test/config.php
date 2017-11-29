c0e12523ac89b721f7ef59d39ec090b635a25c66
bramvandijk <bramvandijk92@gmail.com>
2017-11-29 12:08:54 +0100

    first push

---
+++ test/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+	// BNetAPI->Key
+    'clientId' => "rh2tad2vum2bzshjc8bpaq8werw6su6g",
+
+    // BNetAPI->Secret
+    'clientSecret' => "qChMRM3zRtjEbG7Y96dR2tE5C6BnYmxs",
+
+    // BnetAPI->CallbackURI
+    'redirectUri' => "https://obibramkenobi.github.io/portfolio/droetker/kado.html"
+];
